253dcf4c99b01bbd8f4598987e3f66d613f7a2e7
Jeyaprakash <34285441+turbotrail@users.noreply.github.com>
2019-04-07 21:10:07 +0530

    Add files via upload

---
+++ function graph.py
@@ -0,0 +1,42 @@
+
+# coding: utf-8
+
+# In[65]:
+
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.array([0,45,90,135,180,225,270,315,360])
+y=np.array([0,0,0,0,0,0,0,0,0],dtype=float)
+z=np.array([0,0,0,0,0,0,0,0,0],dtype=float)
+#print(y)
+for e in range(0,8):
+    
+    #print(round(math.sin(math.radians(x[e])),3))
+    y[e]=round(math.sin(math.radians(x[e])),3)
+    z[e]=round(math.cos(math.radians(x[e])),3)
+
+
+#print(y)
+
+plt.xlabel("Angle")
+plt.ylabel("Sine values")
+plt.plot(x,y)
+plt.savefig('sine.pdf', format='pdf')
+plt.show()
+plt.close()
+
+plt.xlabel("Angle")
+plt.ylabel("Cosine values")
+plt.plot(x,z)
+plt.savefig('cosine.pdf', format='pdf')
+plt.show()
+plt.close()
+
+
+# In[33]:
+
+
+print(math.sin(math.radians(90)))
+
