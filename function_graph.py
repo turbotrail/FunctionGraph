3c21e51f1b6fac82807f6e29e961645a198b5311
Jeyaprakash <34285441+turbotrail@users.noreply.github.com>
2021-03-19 01:11:02 +0530

    Update function_graph.py

--- function_graph.py
+++ function_graph.py
@@ -3,7 +3,7 @@
 
 # In[65]:
 
-
+""" This is a simple module to create a sine wave"""
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,31 +12,20 @@ y=np.array([0,0,0,0,0,0,0,0,0],dtype=float)
 z=np.array([0,0,0,0,0,0,0,0,0],dtype=float)
 #print(y)
 for e in range(0,8):
-    
     #print(round(math.sin(math.radians(x[e])),3))
     y[e]=round(math.sin(math.radians(x[e])),3)
     z[e]=round(math.cos(math.radians(x[e])),3)
-
-
 #print(y)
-
 plt.xlabel("Angle")
 plt.ylabel("Sine values")
 plt.plot(x,y)
 plt.savefig('sine.pdf', format='pdf')
 plt.show()
 plt.close()
-
 plt.xlabel("Angle")
 plt.ylabel("Cosine values")
 plt.plot(x,z)
 plt.savefig('cosine.pdf', format='pdf')
 plt.show()
 plt.close()
-
-
-# In[33]:
-
-
 print(math.sin(math.radians(90)))
-
